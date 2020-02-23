a3c2be08919ec3b60261a270fa6e2b410fdb11a7
v_hujinyong <v_hujinyong@baidu.com>
2020-02-23 23:01:10 +0800

    添加url2,新增文件TestB2

--- app/src/main/java/com/example/TestB.java
+++ app/src/main/java/com/example/TestB.java
@@ -6,4 +6,9 @@ public class TestB {
 
 
     public static final String GIT_URL = "https://git-scm.com/book/zh/v2/Git-%E5%9F%BA%E7%A1%80-%E8%AE%B0%E5%BD%95%E6%AF%8F%E6%AC%A1%E6%9B%B4%E6%96%B0%E5%88%B0%E4%BB%93%E5%BA%93";
+
+    public static final String GIT_URL2 = "https://git-scm.com/book/zh/v2/Git-%E5%9F%BA%E7%A1%80-%E8%AE%B0%E5%BD%95%E6%AF%8F%E6%AC%A1%E6%9B%B4%E6%96%B0%E5%88%B0%E4%BB%93%E5%BA%93";
+
+
+
 }
